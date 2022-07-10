c704e5323201c96b9946fabd71a8481921f243ef
Stephen Donahue <sdonahue.metacomet@gmail.com>
2022-07-10 00:41:25 -0400

    fix: add untracked footer / permissions dash

---
+++ src/modules/navigation/components/Footer.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'Footer'
+  });
+</script>
+
+<template>
+  <div id='footer'>
+    Copyright Benchmark Solutions ©{{new Date().getFullYear()}}
+  </div>
+</template>
+
+<style scoped>
+  #footer {
+    height: 300px;
+    background-color: #999;
+    width: 100vw;
+    margin: 0;
+  }
+</style>
---
+++ src/modules/permissions/components/PermissionsDashboard.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'PermissionsDashboard'
+  });
+</script>
+
+<template>
+  <div>
+    <h1>Permissions</h1>
+    <ul>
+      <li>I'm not in love with the admin-admin naming scheme. I've got to find
+        something better.
+      </li>
+      <li>So what can we do as an admin-admin? We can add new users to our org</li>
+      <li>Are we global admin-admins? Or org-specific?</li>
+      <li>We can reset passwords, and perform other mgmt. service functions</li>
+      <li>We can set user and role-level permissions:
+        eg: view-orders, create-orders, modify-orders
+      </li>
+      <li>Are we multi-tenant or single-tenant?</li>
+      <li>Who is the default admin user (is there one?)</li>
+      <li>Should admin-users be distinct from customer-users? If so, how?</li>
+      <li>Would password resets, etc. be better aligned with the customers view?</li>
+      <li>This might be better branded as a permissions view</li>
+    </ul>
+  </div>
+</template>
